3afbb2d2ebd0ae0b9e344062350fb961e31e2db4
inisoliman <one4vc@gmail.com>
2025-11-05 04:00:36 +0200

    Fix syntax error in handlers/__init__.py: remove stray '}'

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,4 @@
-# handlers/__init__.py (patched to import compatibility layer if needed)
+# handlers/__init__.py
 from . import user_handlers
 from . import admin_handlers
 from . import callback_handlers
@@ -14,4 +14,3 @@ def register_all_handlers(bot, channel_id, admin_ids):
     user_handlers.register(bot, channel_id, admin_ids)
     admin_handlers.register(bot, admin_ids)
     callback_handlers.register(bot, admin_ids)
-}
