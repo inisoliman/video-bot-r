88ea47b1d3ce390f026bcb3e9a534f9eaab283c9
inisoliman <one4vc@gmail.com>
2025-11-05 03:37:17 +0200

    تثبيت استيراد handlers بدون تغييرات (no-op) لتثبيت SHA إن لزم

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,16 +1,9 @@
-# handlers/__init__.py
-
-# استيراد الوحدات لجعل دوال التسجيل متاحة
 from . import user_handlers
 from . import admin_handlers
 from . import callback_handlers
 from . import helpers
 
 def register_all_handlers(bot, channel_id, admin_ids):
-    """
-    تقوم هذه الدالة بتسجيل كل المعالجات من الوحدات المنفصلة.
-    """
-    # تمرير الكائنات الضرورية (bot, admin_ids, etc.) لكل دالة تسجيل
     user_handlers.register(bot, channel_id, admin_ids)
     admin_handlers.register(bot, admin_ids)
     callback_handlers.register(bot, admin_ids)
